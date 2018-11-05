54773cb7881a311e80f3bcb05c9d21e5f1d57d45
radriangz <radriangz@outlook.com>
2018-11-05 15:09:47 -0600

    Se creó la clase ReadExcelFileText.java, que toma un archivo excel e imprime el contenido de sus celdas en la consola, pero no funciona.

---
+++ src/com/radrian/ReadAndWrtieExcelFiles/tester/ReadExcelFileText.java
@@ -0,0 +1,42 @@
+package com.radrian.ReadAndWrtieExcelFiles.tester;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.util.Iterator;
+
+import org.apache.poi.ss.usermodel.Cell;
+import org.apache.poi.ss.usermodel.Row;
+import org.apache.poi.xssf.usermodel.XSSFSheet;
+import org.apache.poi.xssf.usermodel.XSSFWorkbook;
+
+public class ReadExcelFileText {
+	public static void main(String[] args) {
+		String fileName = "JIRA_resumen.xlsx";
+		String fileRoute = "C:\\Users\\RAdrian\\Documents\\ExcelTesterFiles\\" + fileName;
+		String fileSheet = "Hoja1";
+		
+		try (FileInputStream file = new FileInputStream(new File(fileRoute))) {
+			XSSFWorkbook workbook = new XSSFWorkbook(file);
+			XSSFSheet sheet = workbook.getSheetAt(0);
+			Iterator<Row> rowIterator = sheet.iterator();
+			
+			Row row;
+			while (rowIterator.hasNext()) {
+				row = rowIterator.next();
+				Iterator<Cell> cellIterator = row.cellIterator();
+				Cell cell;
+				while(cellIterator.hasNext()) {
+					cell = cellIterator.next();
+					System.out.println(cell.getStringCellValue() + " | ");
+				}
+				System.out.println();
+			} 
+			workbook.close();
+		} catch (Exception e) {
+			e.getMessage();
+		}
+		
+	}
+}
