25ce7c449c62391964f9ad9fb64da345df4cdd95
radriangz <radriangz@outlook.com>
2018-11-06 14:20:20 -0600

    Se cambiaron las librerías utilizadas para leer el archivo excel, las que funcionan se agregaron a la carpeta lib/. \nTambién se implementó un try que hace catch de IllegalStateException, esto es porque el método hasNumericValue no puede evaluar a un String, así que el try intenta obtener el valor numérico con getNumericCellValue(), si hay un valor numérico lo imprime en pantalla, pero si no hay valor numérico, lanza el error IllegalStateException que al ser cachado, imprime en pantalla usando el mpetdo getStringCellValue(), aún no sé debería ser el contenido para que fallara el catch. Con esta modificación al ajercicio original que estaba haciendo, el programa ya imprime el contenido de las celdas, sin importar que sean numeros o letras.

--- src/com/radrian/ReadAndWrtieExcelFiles/tester/ReadExcelFileText.java
+++ src/com/radrian/ReadAndWrtieExcelFiles/tester/ReadExcelFileText.java
@@ -16,27 +16,35 @@ public class ReadExcelFileText {
 		String fileName = "JIRA_resumen.xlsx";
 		String fileRoute = "C:\\Users\\RAdrian\\Documents\\ExcelTesterFiles\\" + fileName;
 		String fileSheet = "Hoja1";
-		
+
 		try (FileInputStream file = new FileInputStream(new File(fileRoute))) {
 			XSSFWorkbook workbook = new XSSFWorkbook(file);
 			XSSFSheet sheet = workbook.getSheetAt(0);
 			Iterator<Row> rowIterator = sheet.iterator();
-			
+
 			Row row;
 			while (rowIterator.hasNext()) {
 				row = rowIterator.next();
 				Iterator<Cell> cellIterator = row.cellIterator();
 				Cell cell;
-				while(cellIterator.hasNext()) {
+
+				while (cellIterator.hasNext()) {
 					cell = cellIterator.next();
-					System.out.println(cell.getStringCellValue() + " | ");
+					try {
+						System.out.println(cell.getNumericCellValue() + " | ");
+					} catch (IllegalStateException e) {
+						System.out.println(cell.getStringCellValue() + " | ");
+					}
 				}
 				System.out.println();
-			} 
+			}
 			workbook.close();
-		} catch (Exception e) {
-			e.getMessage();
+		} catch (FileNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
 		}
-		
 	}
 }
