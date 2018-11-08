13c5c4d1d83228f587aa45b138947ad975237cb6
radriangz <radriangz@outlook.com>
2018-11-07 21:49:14 -0600

    Se agregaron algunos ifes anidados, hacen diferentes evaluaciones que permiten iterar la fila de la cabecera del documento, obtener los valores de las columnas que se necesitan, asignarlo a las respectivas variables, con estas se pueden obtener los datos, de las columnas correctas, e imprimirlos en pantalla. Hace falta resolver cómo se va a realizar la sumatoria.

--- src/com/radrian/MainProject/WorkingWithJiraFile/main/MainClass.java
+++ src/com/radrian/MainProject/WorkingWithJiraFile/main/MainClass.java
@@ -6,30 +6,67 @@ import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.util.Iterator;
 
+import org.apache.poi.ss.usermodel.Cell;
+import org.apache.poi.ss.usermodel.CellType;
 import org.apache.poi.ss.usermodel.Row;
+import org.apache.poi.xssf.streaming.SXSSFRow.CellIterator;
 import org.apache.poi.xssf.usermodel.XSSFSheet;
 import org.apache.poi.xssf.usermodel.XSSFWorkbook;
 
 public class MainClass {
+	private static final String ORIGINAL_ESTIMATE = "Estimación original";
+	private static final String RESPONSIBLE = "Responsable";
+	private static final byte HEADER_ROW = 0;
+
 	public static void main(String[] args) {
 		String fileName = "JIRA_resumen.xlsx";
 		String fileRoute = "C:\\Users\\RAdrian\\Documents\\ExcelTesterFiles\\" + fileName;
-		//String fileSheet = "Hoja1";
-		
-		try(FileInputStream file = new FileInputStream(new File(fileRoute))) {
-			XSSFWorkbook workBook = new XSSFWorkbook(file);
-			XSSFSheet sheet = workBook.getSheetAt(0);
+		byte columnOriginalEstimate = 0;
+		byte columnResponsible = 0;
+		// String fileSheet = "Hoja1";
+
+		try (FileInputStream file = new FileInputStream(new File(fileRoute))) {
+			XSSFWorkbook workbook = new XSSFWorkbook(file);
+			XSSFSheet sheet = workbook.getSheetAt(0);
 			Iterator<Row> rowIterator = sheet.iterator();
-			
+
 			Row row;
-			
-			
-			
-		}catch (FileNotFoundException e) {
+			while (rowIterator.hasNext()) {
+				row = rowIterator.next();
+				Iterator<Cell> cellIterator = row.cellIterator();
+				
+				while(cellIterator.hasNext()) {
+					Cell cell = cellIterator.next();
+					
+					if (HEADER_ROW == cell.getRowIndex()) {
+						if(CellType.STRING.equals(cell.getCellType())) {
+							if(RESPONSIBLE.equals(cell.getStringCellValue())) {
+								columnResponsible = (byte) cell.getColumnIndex();
+							} else if(ORIGINAL_ESTIMATE.equals(cell.getStringCellValue())) {
+								columnOriginalEstimate = (byte) cell.getColumnIndex();
+							} else {
+								continue;
+							}
+						}
+					}
+					if (columnResponsible == cell.getColumnIndex()) {
+						System.out.println("Responsable: " + cell.getStringCellValue());
+					} else if (columnOriginalEstimate == cell.getColumnIndex()) {
+						if(CellType.STRING.equals(cell.getCellType())) {
+							System.out.println("Estimación original " + cell.getStringCellValue());
+						} else if (CellType.NUMERIC.equals(cell.getCellType())) {
+							System.out.println("Estimación original " + cell.getNumericCellValue());
+						} else {
+							continue;
+						}
+					}
+				}
+			}
+			workbook.close();
+		} catch (FileNotFoundException e) {
 			e.printStackTrace();
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
-
 	}
 }
