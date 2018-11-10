a748c2042a999d16c0a4b1edd9f743318a0e9051
radriangz <radriangz@outlook.com>
2018-11-09 23:23:32 -0600

    Se renombró el paquete para cumplir con la convención. Se crearon nuevos métodos para calcular horas y días en la clase  Empleado, el méetodo empleado se quedó con solo dos constructores, se elliminaron algunos setters y se crearon dos métodos que suman una valor nuevo al valor guardado en las variables de la clase. La clase MainClass se renombró a SearchExcelFile.

--- src/com/radrian/MainProject/WorkingWithJiraFile/Classes/Empleado.java
+++ src/com/radrian/MainProject/WorkingWithJiraFile/Classes/Empleado.java
@@ -1,57 +1,59 @@
-package com.radrian.MainProject.WorkingWithJiraFile.Classes;
+package com.radrian.mainproject.workingwithjirafile.classes;
 
 /**<h1>Empleado</h1>
- * Creates an Empleado type Object.
+ * Creates an Empleado type Object. Stores three parameters.
+ * Can convert the time parameters into hours and days.
+ * Can add new values to existing time values.
  * 
- * @param name
- * @param originalEstimation - Time originally considered
- * @
+ * @param name - Employee's name.
+ * @param originalEstimate - Originally considered time.
+ * @param remainingEstimate - What remains of the originally considered time.
  * 
  * @author RAdrian
  */
 public class Empleado {
 	private String name;
-	private int originalEstimate;
-	private int remainingEstimate;
+	private double originalEstimate;
+	private double remainingEstimate;
 	
-	public Empleado(String name) {
+	public Empleado(String name, double originalEstimate) {
 		this.name = name;
-	}
-	
-	public Empleado(String name, int originalEstimate) {
-		this(name);
 		this.originalEstimate = originalEstimate;
+		this.remainingEstimate = 0;
 	}
 
-	public Empleado(String name, int originalEstimate, int remainingEstimate) {
+	public Empleado(String name, double originalEstimate, double remainingEstimate) {
 		this(name, originalEstimate);
 		this.remainingEstimate = remainingEstimate;
 	}
 
+	public void addToOriginalEstimate(float value) {
+		this.originalEstimate += value;
+	}
+	
+	public void addToRemainingEstimate(float value) {
+		this.remainingEstimate += value;
+	}
+	
 	public String getName() {
 		return name;
 	}
 
-	public void setName(String name) {
-		this.name = name;
-	}
-
-	public int getOriginalEstimate() {
+	public double getOriginalEstimate() {
 		return originalEstimate;
 	}
-
-	public void setOriginalEstimate(int seconds) {
-		this.originalEstimate = seconds;
-	}
-	
-	public int getRemainingEstimate() {
+	public double getRemainingEstimate() {
 		return remainingEstimate;
 	}
 
-	public void setRemainingEstimate(int seconds) {
-		this.remainingEstimate = seconds;
+	public float convertToHours(float secondsToHours) {
+		return (secondsToHours/3600);
 	}
-
+	
+	public float convertToLaboralDays(float secondsToDays) {
+		return (convertToHours(secondsToDays)/8);
+	}
+	
 	@Override
 	public String toString() {
 		// TODO Auto-generated method stub
--- src/com/radrian/MainProject/WorkingWithJiraFile/main/SearchExcelFile.java
+++ src/com/radrian/MainProject/WorkingWithJiraFile/main/SearchExcelFile.java
@@ -1,4 +1,4 @@
-package com.radrian.MainProject.WorkingWithJiraFile.main;
+package com.radrian.mainproject.workingwithjirafile.main;
 
 import java.io.File;
 import java.io.FileInputStream;
